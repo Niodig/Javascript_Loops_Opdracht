9353d25b8afe52aa4a7d2a0f017bddce7020608d
Niodig <steur81@gmail.com>
2022-10-23 14:26:35 +0200

    opdracht voltooid

--- loops.js
+++ loops.js
@@ -1,39 +1,62 @@
-// ==========================================
-// Opdracht 1. Maak een for-loop die 3 keer "Hoera!" in de terminal logt
-// Verwachte uitkomsten:
-// Hoera!
-// Hoera!
-// Hoera!
-// ==========================================
-
-
-
-// ==========================================
-// Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
-// Verwachte uitkomsten:
-// loop...
-// loop...
-// loop...
-// loop...
-// klaar!
- // ==========================================
-
-
-
-// ==========================================
-// Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
-// Verwachte uitkomsten:
-// 0 is even
-// 1 is oneven
-// 2 is even
-// 3 is oneven
-// .... etc.
-// 15 is oneven
-
-// Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
-// ==========================================
-
-
+// // ==========================================
+// // Opdracht 1. Maak een for-loop die 3 keer "Hoera!" in de terminal logt
+// // Verwachte uitkomsten:
+// // Hoera!
+// // Hoera!
+// // Hoera!
+// // ==========================================
+//
+for (let i = 0; i < 3; i++) {
+    console.log('Hoera!');
+}
+//
+//
+// // ==========================================
+// // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
+// // Verwachte uitkomsten:
+// // loop...
+// // loop...
+// // loop...
+// // loop...
+// // klaar!
+//  // ==========================================
+//
+for (let i = 0; i < 5; i++) {
+    if (i === 4) {
+        console.log('Klaar!')
+    } else {
+        console.log('Loop...')
+    }
+}
+//
+//
+//
+// // ==========================================
+// // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
+// // Verwachte uitkomsten:
+// // 0 is even
+// // 1 is oneven
+// // 2 is even
+// // 3 is oneven
+// // .... etc.
+// // 15 is oneven
+//
+// // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
+// // ==========================================
+//
+//
+for (let i = 0; i<16; i++) {
+    if (i % 2 === 0){
+        console.log(i+ ' is' + ' even');
+    }
+    else{
+        console.log(i + ' is' + ' odd');
+    }
+}
+//     // string interpolatie
+// //     for (let i = 0; i<16; i++) {
+// //     console.log(`${i} is ${i % 2 == 0 ? 'even' : 'odd'}`);
+// }
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -51,3 +74,24 @@
 // >> 9
 // ==========================================
 
+for (let i = 0; i > 10; i++) {
+    if (i < 3){
+        console.log(i);
+    }
+    if (i >= 3 && i < 6) {
+        console.log('> ' + i);
+    }
+    if (i >= 6) {
+        console.log('>> ' + i);
+    }
+}
+
+for (let i = 0; i < 10; i++) {
+    if (i >= 3 && i < 6) {
+        console.log('> ' + i);
+    } else if (i >= 6) {
+        console.log('>> ' + i);
+    } else {
+        console.log(i);
+    }
+}
